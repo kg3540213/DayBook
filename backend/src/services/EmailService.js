e884667b61661452f0026bbfeb20f2793cdfa20c
Koushik Ghosh <kg3540213@gmail.com>
2026-03-26 00:51:22 +0530

    changes

--- backend/src/services/EmailService.js
+++ backend/src/services/EmailService.js
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
-  secure: true, // Use SSL instead of TLS
+  secure: false, // Use SSL instead of TLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
